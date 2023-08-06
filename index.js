058931b0304bf01d43c1dcacba33c551cf5a9d9e
eduzgun <emreduz00@gmail.com>
2023-08-06 17:30:19 +0100

    svg hover

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-var paths = document.querySelectorAll("section > svg > path"),
+/*
+var paths = document.querySelectorAll("section > div > svg > path "),
     i=0;
 
 paths.forEach(function(item,index){
@@ -13,7 +14,48 @@ paths.forEach(function(item,index){
     //item.innerHTML = "<animate attributeName='stroke-dashoffset' begin='0s' dur='3s' to='0' fill='freeze' />";
     item.setAttribute("class", "new")
     console.log(item, index, pathLength, item.innerHTML)
-    
+    console.log(item.z-index)
+    console.log("to")
+
 });
 
-console.log("to")
+*/
+var svg = document.querySelector(".svg-container");
+var pathGroup = svg.querySelector(".path-group");
+var paths = Array.from(pathGroup.querySelectorAll("path"));
+
+function bringToFront(path) {
+    pathGroup.removeChild(path);
+    pathGroup.appendChild(path);
+}
+
+function restoreOrder(path) {
+    // Check if the path is still a child of pathGroup before reinserting
+    if (!pathGroup.contains(path)) {
+        pathGroup.appendChild(path);
+    }
+}
+
+paths.forEach(function (item) {
+    var pathLength = item.getTotalLength();
+    item.setAttribute("stroke-dasharray", pathLength);
+    item.setAttribute("stroke-dashoffset", pathLength);
+    item.classList.add("new");
+
+    item.addEventListener("mouseover", function () {
+        bringToFront(item);
+    });
+
+    item.addEventListener("mouseout", function () {
+        restoreOrder(item);
+    });
+});
+
+let svg2 = document.querySelector("#svg_2")
+let svg3 = document.querySelector("#svg_3")
+
+svg2.addEventListener("click", function() {
+    console.log("clicked") 
+});
+
+
